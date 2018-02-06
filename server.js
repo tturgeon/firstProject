e476ecb8cb4ac76683704900c31df14d47777798
Trevor Turgeon <trevorturgeon@Trevors-MacBook-Pro.local>
2018-02-06 10:23:50 -0700

    Added port to server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 3000;
 app.use('/',express.static(path.join(__dirname, 'public')));
-app.listen(3000,()=>console.log("it works 3000"))
+app.listen(port,()=>console.log("Served on " + port))
